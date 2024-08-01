95e7e1bf88d7e8722be588bcb64300602c95ca3d
Alexandr <romanenyasasha@gmail.com>
2024-08-01 16:54:23 +0300

    Retry added

--- terminate-all-screens.sh
+++ terminate-all-screens.sh
@@ -1,22 +1,41 @@
-#!/bin/bash
-
-# This script terminates all screen sessions with a specified name, locally or on a remote server via SSH
-
-# Function to terminate screen sessions by name
 terminate_screens() {
   local name="$1"
   local user_host="$2"
+  local max_retries=5  
+  local retry_delay=2 
 
-  if [ -n "$user_host" ]; then
-    # Command to execute on remote server
-    ssh "$user_host" "screen -ls | grep '\.$name' | awk '{print \$1}' | xargs -I{} screen -S {} -X quit"
-  else
-    # Command to execute locally
-    screen -ls | grep "\.$name" | awk '{print $1}' | xargs -I{} screen -S {} -X quit
-  fi
+  terminate() {
+    if [ -n "$user_host" ]; then
+      ssh "$user_host" "screen -ls | grep '\.$name' | awk '{print \$1}' | xargs -I{} screen -S {} -X quit"
+    else
+      screen -ls | grep "\.$name" | awk '{print $1}' | xargs -I{} screen -S {} -X quit
+    fi
+  }
+
+  screens_exist() {
+    if [ -n "$user_host" ]; then
+      ssh "$user_host" "screen -ls | grep '\.$name'" > /dev/null 2>&1
+    else
+      screen -ls | grep "\.$name" > /dev/null 2>&1
+    fi
+  }
+
+  local attempt=1
+  while [ $attempt -le $max_retries ]; do
+    terminate
+
+    sleep "$retry_delay"
+
+    if ! screens_exist; then
+      return 0
+    fi
+
+    ((attempt++))
+  done
+
+  return 1
 }
 
-# Check if a screen name is provided
 if [ -z "$1" ]; then
   echo "Usage: $0 <screen_name> [<user>@<host>]"
   exit 1
@@ -25,7 +44,4 @@ fi
 SCREEN_NAME="$1"
 USER_HOST="$2"
 
-# Terminate the screens
 terminate_screens "$SCREEN_NAME" "$USER_HOST"
-
-echo "All screen sessions with the name '$SCREEN_NAME' have been terminated."
