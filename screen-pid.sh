8dcffb49c6f8b2a548ca4083199923505d005d3b
Alexandr <romanenyasasha@gmail.com>
2024-08-02 14:31:38 +0300

    Added new script: screen-pid.sh

---
+++ screen-pid.sh
@@ -0,0 +1,26 @@
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+    echo "Usage: $0 <screen_name> [ssh_host]"
+    exit 1
+fi
+
+screen_name=$1
+ssh_host=$2
+
+if [ -z "$ssh_host" ]; then
+    screen_command="screen -ls"
+else
+    screen_command="ssh $ssh_host 'screen -ls'"
+fi
+
+pid=$(eval "$screen_command" | awk -v name="$screen_name" '
+    /Detached/ {
+        split($1, arr, ".")
+        if (arr[2] == name) print arr[1]
+    }')
+
+if [ -z "$pid" ]; then
+    echo "No 'screen' process found with name $screen_name."
+    exit 1
+else
+    echo "The PID of 'screen' with name $screen_name is: $pid"
+fi
